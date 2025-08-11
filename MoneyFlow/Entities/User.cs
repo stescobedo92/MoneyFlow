4e22b75cdc5a24b24e2de76458c84af8c229be50
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2025-08-11 08:17:17 -0600

    Add Service class and update User class for relationships
    Introduced a new `Service` class in the `MoneyFlow.Entities` namespace with properties for `ServiceId`, `UserId`, `Name`, `Type`, and a navigation property for `User`. Updated the `User` class to include a `Services` collection, establishing a one-to-many relationship between `User` and `Service`.

--- MoneyFlow/Entities/User.cs
+++ MoneyFlow/Entities/User.cs
@@ -6,4 +6,6 @@ public class User
     public string FullName { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
     public string Password { get; set; } = string.Empty;
+
+    public ICollection<Service> Services { get; set; }
 }
