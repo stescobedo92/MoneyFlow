9187afabb25ade24b4befb3474bd10cec69d370d
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2025-08-11 11:26:25 -0600

    Refactor service retrieval and update view model
    Updated ServiceManager to return ServiceViewModel instead of Service. Added ServiceViewModel class to define the view model structure. Modified Index.cshtml to use the new model type and enhanced the layout with action buttons for services.

--- MoneyFlow/Managers/ServiceManager.cs
+++ MoneyFlow/Managers/ServiceManager.cs
@@ -1,10 +1,19 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using MoneyFlow.Context;
 using MoneyFlow.Entities;
+using MoneyFlow.Models;
 
 namespace MoneyFlow.Managers;
 
 public class ServiceManager(AppDbContext _dbContext)
 {
-    public IEnumerable<Service> GetAll(int userId) => _dbContext.Services.Where(user => user.UserId.Equals(userId)).ToList();
+    public IEnumerable<ServiceViewModel> GetAll(int userId) => _dbContext.Services.Where(user => user.UserId.Equals(userId)).
+                                                     Select(serviceVM => new ServiceViewModel
+                                                     {
+                                                        ServiceId = serviceVM.ServiceId,
+                                                        UserId = serviceVM.UserId,
+                                                        Name = serviceVM.Name,
+                                                        Type = serviceVM.Type
+                                                     })
+                                                     .ToList();
 }
---
+++ MoneyFlow/Models/ServiceViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MoneyFlow.Models;
+
+public class ServiceViewModel
+{
+    public int ServiceId { get; set; }
+    public int UserId { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Type { get; set; } = string.Empty;
+}
